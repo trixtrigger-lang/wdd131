eaf9c8ccbd5c87d72b40bfa6c2ddc7ea4cc53e72
Paul Ethan <trixtrigger@gmail.com>
2026-01-17 21:00:11 +0300

    tf

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// Current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Last modified date
+document.getElementById("lastModified").textContent =
+    "Last Modified: " + document.lastModified;
---
+++ week01/practice/java.js
@@ -0,0 +1,10 @@
+let myname;
+let myage;
+
+myname = "dave";
+myage = 22;
+
+myname;
+myage;
+
+let mydog = "blek";
