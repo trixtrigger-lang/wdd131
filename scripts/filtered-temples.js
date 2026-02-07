62591f3f5f5a6494ea37cd51673f6bcd0b6b01ba
Paul Ethan <trixtrigger@gmail.com>
2026-02-07 20:16:11 +0300

    don

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -1,74 +1,74 @@
 // Temple Array
 const temples = [
     {
-        templeName: "Aba Nigeria",
-        location: "Aba, Nigeria",
-        dedicated: "2005, August, 7",
-        area: 11500,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+        templeName: "London England",
+        location: "London, England",
+        dedicated: "1958, September, 7-9",
+        area: 42652,
+        imageUrl: "images/londonenglandtemple.jpg"
     },
     {
-        templeName: "Manti Utah",
-        location: "Manti, Utah, United States",
-        dedicated: "1888, May, 21",
-        area: 74792,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+        templeName: "Manila Philippines",
+        location: "Manila Philipines",
+        dedicated: "1984, September, 4-15",
+        area: 26683,
+        imageUrl: "images/manilaphillipinestemple.jpg"
     },
     {
-        templeName: "Payson Utah",
-        location: "Payson, Utah, United States",
-        dedicated: "2015, June, 7",
-        area: 96630,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+        templeName: "Paris France",
+        location: "Paris, France",
+        dedicated: "2017, May, 21",
+        area: 44175,
+        imageUrl: "images/parisfrancetemple.jpg"
     },
     {
-        templeName: "Yigo Guam",
-        location: "Yigo, Guam",
-        dedicated: "2020, May, 2",
-        area: 6861,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+        templeName: "Rome Italy",
+        location: "Rome, Italy",
+        dedicated: "2019, March, 12",
+        area: 41010,
+        imageUrl: "images/romeitalytemple.jpg"
     },
     {
-        templeName: "Washington D.C.",
-        location: "Kensington, Maryland, United States",
-        dedicated: "1974, November, 19",
-        area: 156558,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+        templeName: "Saltlake Utah",
+        location: "Saltlake, Utah, United States",
+        dedicated: "1893, April, 24",
+        area: 382207,
+        imageUrl: "images/saltlaketemple.jpg"
     },
     {
-        templeName: "Lima Perú",
-        location: "Lima, Perú",
-        dedicated: "1986, January, 10",
-        area: 9600,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+        templeName: "Sydney Australia",
+        location: "Sydney, Australia",
+        dedicated: "1984, September, 23",
+        area: 30067,
+        imageUrl: "images/sydneyaustraliatemple.jpg"
     },
     {
-        templeName: "Mexico City Mexico",
-        location: "Mexico City, Mexico",
-        dedicated: "1983, December, 2",
-        area: 116642,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+        templeName: "Tokyo Japan",
+        location: "Tokyo, Japan",
+        dedicated: "1980, October, 29",
+        area: 53997,
+        imageUrl: "images/tokyojapantemple.jpg"
     },
     {
         templeName: "Nairobi Kenya",
         location: "Nairobi, Kenya",
         dedicated: "2023, May, 21",
         area: 19800,
-        imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/nairobi-kenya-temple/nairobi-kenya-temple-56338.jpg"
+        imageUrl: "images/nairobikenyatemple.jpg"
     },
     {
         templeName: "Accra Ghana",
         location: "Accra, Ghana",
         dedicated: "2004, January, 11",
         area: 17500,
-        imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/accra-ghana-temple/accra-ghana-temple-54135.jpg"
+        imageUrl: "images/accraghanatemple.jpg"
     },
     {
         templeName: "Paris France",
         location: "Paris, France",
         dedicated: "2017, May, 21",
-        area: 44000,
-        imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/paris-france-temple/paris-france-temple-37038.jpg"
+        area: 44175,
+        imageUrl: "images/parisfrancetemple.jpg"
     }
 ];
 
